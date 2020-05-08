b3633a67638b50d8ff2fdc79667cb9a4a962fdc5
ChristianEllerbrock <christian.ellerbrock@inbayern.net>
2020-05-09 01:31:41 +0200

    2020.05.09

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -3,11 +3,23 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type Tenant {
     _id: ID
-    dbName: String
     name: String
+    adminEmail: String
+    dbName: String
   }
+
+  input CreateTenantInput {
+    name: String
+    adminEmail: String
+    dbName: String
+  }
+
   type Query {
     getAllTenants: [Tenant]
+  }
+
+  type Mutation {
+    createTenant(input: CreateTenantInput): Tenant
   }
 `;
 
